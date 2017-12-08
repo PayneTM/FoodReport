5bc7348bf1fed3088dc4c4689d827c8f59c5c142
PayneTM <maksim.tantsyura@gmail.com>
2017-12-08 20:28:00 +0200

    Feature is ready

--- FoodReport/wwwroot/js/ReportCreate.js
+++ FoodReport/wwwroot/js/ReportCreate.js
@@ -2,96 +2,88 @@
 var isNewRowAllowed = true;
 var list = [];
 function Add() {
-    if (NotEmptyInputs())
-    {
+    if (NotEmptyInputs()) {
 
-    //make inputs
-    ///////////////////////////////////////
-    //Product input
-    //<input class="form-control" type="text" id = product# placeholder="Product">
-    //////////////////////////////////////
+        //make inputs
+        ///////////////////////////////////////
+        //Product input
+        //<input class="form-control" type="text" id = product# placeholder="Product">
+        //////////////////////////////////////
         //var input1 = document.createElement("input");
         var input1 = document.createElement("select");
-    input1.className = "form-control";
-    input1.type = "text";
-    input1.id = "prod" + counter;
-    for (var i = 0; i != list.length; i++)
-    {
-        var rest = list[0][0].name;
-        var opt = document.createElement("option");
-        opt.innerHTML = rest;
-        input1.options.add(opt);
-    }
-    //input1.options.add(list);
-    //input1.placeholder = "Product";
+        input1.className = "form-control";
+        input1.type = "text";
+        input1.id = "prod" + counter;
+        for (var i = 0; i != list[0].length; i++) {
+            var rest = list[0][i].name;
+            var opt = document.createElement("option");
+            opt.innerHTML = rest;
+            input1.options.add(opt);
+        }
 
-    ///////////////////////////////////////
-    //Count input
-    //<input class="form-control" type="text" id = count# placeholder="Count">
-    //////////////////////////////////////
-    var input2 = document.createElement("input");
-    input2.className = "form-control";
-    input2.type = "number";
-    input2.id = "count" + counter;
-    input2.placeholder = "Count";
+        ///////////////////////////////////////
+        //Count input
+        //<input class="form-control" type="text" id = count# placeholder="Count">
+        //////////////////////////////////////
+        var input2 = document.createElement("input");
+        input2.className = "form-control";
+        input2.type = "number";
+        input2.id = "count" + counter;
+        input2.placeholder = "Count";
 
-    ///////////////////////////////////////
-    //Price input
-    //<input class="form-control" type="text" id = price# placeholder="Price">
-    //////////////////////////////////////
-    var input3 = document.createElement("input");
-    input3.className = "form-control";
-    input3.type = "number";
-    input3.id = "price" + counter;
-    input3.placeholder = "Price";
+        ///////////////////////////////////////
+        //Price input
+        //<input class="form-control" type="text" id = price# placeholder="Price">
+        //////////////////////////////////////
+        var input3 = document.createElement("input");
+        input3.className = "form-control";
+        input3.type = "number";
+        input3.id = "price" + counter;
+        input3.placeholder = "Price";
 
 
-    //make divs for inputs
-    var divinput1 = document.createElement("div");
-    divinput1.className = "col-sm-4";
-    divinput1.style.paddingTop = "5px";
-    divinput1.appendChild(input1);
+        //make divs for inputs
+        var divinput1 = document.createElement("div");
+        divinput1.className = "col-sm-4";
+        divinput1.style.paddingTop = "5px";
+        divinput1.appendChild(input1);
 
-    var divinput2 = document.createElement("div");
-    divinput2.className = "col-sm-4";
-    divinput2.style.paddingTop = "5px";
+        var divinput2 = document.createElement("div");
+        divinput2.className = "col-sm-4";
+        divinput2.style.paddingTop = "5px";
 
-    divinput2.appendChild(input2);
+        divinput2.appendChild(input2);
 
-    var divinput3 = document.createElement("div");
-    divinput3.className = "col-sm-4";
-    divinput3.style.paddingTop = "5px";
+        var divinput3 = document.createElement("div");
+        divinput3.className = "col-sm-4";
+        divinput3.style.paddingTop = "5px";
 
-    divinput3.appendChild(input3);
+        divinput3.appendChild(input3);
 
 
-    //get global div
-    var cont = document.getElementById("container");
+        //get global div
+        var cont = document.getElementById("container");
 
-    var block = document.createElement("div");
-    block.id = "blockInput" + counter;
-    block.appendChild(document.createElement("br"));
-    block.appendChild(divinput1);
-    block.appendChild(divinput2);
-    block.appendChild(divinput3);
-    cont.appendChild(block);
-    counter++;
+        var block = document.createElement("div");
+        block.id = "blockInput" + counter;
+        block.appendChild(document.createElement("br"));
+        block.appendChild(divinput1);
+        block.appendChild(divinput2);
+        block.appendChild(divinput3);
+        cont.appendChild(block);
+        counter++;
     }
 }
 
-function NotEmptyInputs()
-{
+function NotEmptyInputs() {
     var OK = true;
     var inputs = document.getElementsByTagName("input");
-    for (var i = 1; i != inputs.length; i++)
-    {
-        if (inputs.item(i).value.length == 0)
-        {
+    for (var i = 1; i != inputs.length; i++) {
+        if (inputs.item(i).value.length == 0) {
             inputs.item(i).style.borderColor = "red";
             OK = false;
         }
-        else
-        {
+        else {
             inputs.item(i).style.borderColor = "silver";
         }
     };
@@ -100,19 +92,21 @@ function NotEmptyInputs()
 
 function Send() {
     var inputs = document.getElementsByTagName("input");
-    var count = document.getElementsByClassName("form-control")
+    var opt = document.getElementsByTagName("select");
 
     var result = [];
-    for (var i = 1; i < inputs.length; i = i + 3) {
+    for (var i = 1, j = 0; i < inputs.length; i = i + 2, j++) {
         var field =
             {
                 product: '',
                 count: 0,
                 price: 0
             };
-        field.product = inputs.item(i).value;
-        field.count = inputs.item(i + 1).value;
-        field.price = inputs.item(i + 2).value;
+        if (j != opt.length) {
+            field.product = opt[j].value;
+        }
+        field.count = inputs.item(i).value;
+        field.price = inputs.item(i + 1).value;
         result.push(field);
     };
     var leng = result.toString().length;
@@ -127,8 +121,7 @@ function Send() {
             success: window.location.href = '/api/report/all'
         });
 }
-function GetProds()
-{
+function GetProds() {
     var product =
         {
             id: "",
@@ -137,23 +130,8 @@ function GetProds()
             price: 0,
             provider: "",
             unit: ""
-        }; 
-    //$.ajax(
-    //    {
-    //        type: "GET",
-    //        url: "/api/product/prodsjson",
-    //        data: prods,
-    //        contentType: "application/json; charset=utf-8",
-    //        dataType: "json",
-    //        success: function (data)
-    //        {
-    //            prods = JSON.parse(data);
-    //            alert(prods);
-    //        }
-    //    });
+        };
     $.getJSON("/api/product/prodsjson", product, function (product) {
         list.push(product);
-        //var smth = prods[0].id;
-        //alert(prods[0].id);
     });
 }
